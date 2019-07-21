5576c5739fcd7d7aa7f84260e95546770487e2f6
k8tems <k8tems@live.com>
2019-07-21 10:53:50 +0900

    Fix index

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ if __name__ == "__main__":
     if len(argv) != 3:
         print("Wrong arguments.")
     else:
-        text = argv[2]
-        n_secs = int(argv[1])
+        text = argv[1]
+        n_secs = int(argv[2])
 
         dest = '%s__%dsecs.wav' % (text, n_secs)
         tts(text, dest)
