41fcf69ed20c35a075f0b54c0cb071c7173d3208
k8tems <k8tems@live.com>
2019-07-21 10:16:32 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, division
+import scipy.io.wavfile as wavf
+import numpy as np
+from sys import argv
+
+
+def pad_audio(data, n_pad):
+    shape = (n_pad,) + data.shape[1:]
+    if shape[0] > 0:
+        p = np.zeros(shape, np.int16)  # キャストが無いと音声再生不可
+        return np.vstack((p, data)) if len(shape) > 1 else np.hstack((p, data))
+    else:
+        return data
+
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Wrong arguments.")
+        print("Use: %s in.wav out.wav target_time_s" % argv[0])
+    else:
+        in_wav = argv[1]
+        out_wav = argv[2]
+        n_secs = float(argv[3])
+        sample_rate, in_data = wavf.read(in_wav)
+        print("Padding with %s seconds of silence" % str(n_secs))
+        out_data = pad_audio(in_data, int(sample_rate * n_secs))
+        wavf.write(out_wav, sample_rate, out_data)
